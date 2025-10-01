81089b3b651b0dfec4257b239046eea391c11bf9
EvstigneevNV <evstigneevnv@answer-42.ru>
2025-10-01 22:59:33 +0300

    hw09

---
+++ hw09/src/main/java/ru/otus/hw/controller/CommentController.java
@@ -0,0 +1,43 @@
+package ru.otus.hw.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ru.otus.hw.services.CommentService;
+
+@RequiredArgsConstructor
+@Controller
+@RequestMapping("/book/{bookId}/comments")
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping
+    public String add(@PathVariable Long bookId,
+                      @RequestParam String text,
+                      RedirectAttributes ra) {
+        commentService.insert(text, bookId);
+        ra.addFlashAttribute("msg", "Комментарий добавлен");
+        return "redirect:/book/" + bookId;
+    }
+
+    @PostMapping("/{id}/update")
+    public String update(@PathVariable Long bookId,
+                         @PathVariable Long id,
+                         @RequestParam String text,
+                         RedirectAttributes ra) {
+        commentService.update(id, text, bookId);
+        ra.addFlashAttribute("msg", "Комментарий обновлён");
+        return "redirect:/book/" + bookId;
+    }
+
+    @PostMapping("/{id}/delete")
+    public String delete(@PathVariable Long bookId,
+                         @PathVariable Long id,
+                         RedirectAttributes ra) {
+        commentService.deleteById(id);
+        ra.addFlashAttribute("msg", "Комментарий удалён");
+        return "redirect:/book/" + bookId;
+    }
+}
